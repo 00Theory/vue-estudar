d803578fe6a8e7bd2b7cb4f60cce5e7bf1a59cda
Marco Lima <marco200002010@outlook.com>
2024-10-01 14:32:37 -0300

    Job v-for resolvido

--- vue-project/src/components/JobListings.vue
+++ vue-project/src/components/JobListings.vue
@@ -2,19 +2,21 @@
 import jobData from '@/jobs.json';
 import { ref } from 'vue';
 
-const jobs = ref(jobData);
+const jobs = ref(jobData.jobs);
+
+console.log(jobData)
+console.log(jobData.jobs)
 </script>
 
 <template>
     <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto">
             <h2 class="text-3x1 font-bold text-green-500 mb-6 text-center">
-                Browse Jobs
+                Browse Jobs 
             </h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <div v-for="job in jobs" :key="job.id">
-                    Títulos de Cargos:
-                    {{ job.title }}
+                      {{ job.title }}
                 </div>
             </div>
         </div>
